2ed5587eda4e54d92ed59c7702e8e0226312c2df
VishnuBhanderi <vishnubhanderi0712@gmail.com>
2023-08-10 18:48:08 +0530

    Date - 10/08/2023

---
+++ Leetcode/75.sort-colors.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=75 lang=cpp
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+// @lc code=end
